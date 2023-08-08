9fdd41a5c85c20ea3e9be919ea6aa1679d0066ac
patrickhum <jirapat_na@cmu.ac.th>
2023-08-08 13:43:11 +0700

    6.10

--- lab2/src/views/EventListView.vue
+++ lab2/src/views/EventListView.vue
@@ -2,40 +2,13 @@
 import EventCard from '../components/EventCard.vue'
 import type { EventItem } from '@/type'
 
-import { ref } from 'vue'
-const events = ref<EventItem[]>([
-  {
-    id: 5928101,
-    category: 'animal welfare',
-    title: 'Cat Adoption Day',
-    description: 'Find your new feline friend at this event.',
-    location: 'Meow Town',
-    date: 'January 28, 2022',
-    time: '12:00',
-    organizer: 'Kat Laydee'
-  },
-  {
-    id: 4582797,
-    category: 'food',
-    title: 'Community Gardening',
-    description: 'Join us as we tend to the community edible plants.',
-    location: 'Flora City',
-    date: 'March 14, 2022',
-    time: '10:00',
-    organizer: 'Fern Pollin'
-  },
-  {
-    id: 8419988,
-    category: 'sustainability',
-    title: 'Beach Cleanup',
-    description: 'Help pick up trash along the shore.',
-    location: 'Playa Del Carmen',
-    date: 'July 22, 2022',
-    time: '11:00',
-    organizer: 'Carey Wales'
-  }
+import { type Ref, ref } from 'vue'
+import EventService from '@/services/EventService'
+const events: Ref<Array<EventItem[]>> = ref([])
 
-])
+EventService.getEvent().then((response) => {
+    events.value = response.data
+  })
 </script>
 
 <template>
@@ -46,7 +19,7 @@ const events = ref<EventItem[]>([
 </template>
 
 <style scoped>
-.events{
+.events {
   display: flex;
   flex-direction: column;
   align-items: center;
